93f6119707ddac3266bb49339af2f1ca68361c9f
kimji9663 <rlawlgp9663@naver.com>
2021-10-18 00:25:48 +0900

    NEXTJS 입문자를 위한 4주 강의 1주차

--- caffe/components/Header.js
+++ caffe/components/Header.js
@@ -5,21 +5,21 @@ export default function Header() {
       <header className="flex flex-row p-2 justify-between">
         <div>
           <Link href="/">
-            <button type="button" class="btn btn-link">Link</button>
+            <button type="button" className="btn btn-link">Link</button>
           </Link>
         </div>
         <div>
           <Link href="/introduce">
-            <button type="button" class="btn btn-link">Introduce</button>
+            <button type="button" className="btn btn-link">Introduce</button>
           </Link>
           <Link href="/order">
-          <button type="button" class="btn btn-link">Order</button>
+          <button type="button" className="btn btn-link">Order</button>
           </Link>
           <Link href="/hiring">
-          <button type="button" class="btn btn-link">Hiring</button>
+          <button type="button" className="btn btn-link">Hiring</button>
           </Link>
           <Link href="/contactUs">
-          <button type="button" class="btn btn-link">Contect Us</button>
+          <button type="button" className="btn btn-link">Contect Us</button>
           </Link>
         </div>
       </header>
--- caffe/pages/order.js
+++ caffe/pages/order.js
@@ -1,12 +1,60 @@
 import Link from 'next/link';
 import Header from '../components/Header';
+import { useState } from 'react';
 
-export default function Home() {
+// 한화 화폐 단위 포멧(세자릿수마다 콤마)
+const formatter = Intl.NumberFormat( 'ko-KR' );
+
+// 상태 State
+export default function Order() {
+  console.log('함수컴포넌트 시작');
+  let number = 0;
+  console.log( 'number : ', number );
+  number++;
+  console.log( 'number : ', number );
+  
+  // [ 일기전용, 쓰기전용 ] = useState( 기본값 );
+  const [ hasEspresso, setEspresso ] = useState( false );
+  const [ hasAmericano, setAmericano ] = useState( false );
+  const [ hasLatte, setLatte ] = useState( false );
+  let sum = 0;
+  sum += hasEspresso ? 2800 : 0;
+  sum += hasAmericano ? 3000 : 0;
+  sum += hasLatte ? 3500 : 0;
+
+  console.log('랜더링 시작');
+
   return <>
     <div className="container">
         <Header/>
         
         <h1>Order</h1>
+        <h2>메뉴판</h2>
+
+        <dl>
+          <dt>에스프레소</dt>
+          <dd>2,800원 
+            <small><button onClick={ () => setEspresso( !hasEspresso ) } type="button">[ { hasEspresso ? '선택 해제' : '선택'} ]</button></small>
+          </dd>
+
+          <dt>아메리카노</dt>
+          <dd>3,000원 
+            <small><button onClick={ () => setAmericano( !hasAmericano ) } type="button">[ { hasAmericano ? '선택 해제' : '선택'} ]</button></small>
+          </dd>
+
+          <dt>카페라떼</dt>
+          <dd>3,500원 
+            <small><button onClick={ () => setLatte( !hasLatte ) } type="button">[ { hasLatte ? '선택 해제' : '선택'} ]</button></small>
+          </dd>
+
+          <hr/>
+
+          <h2>주문서</h2>
+          <ul className="list-unstyled">
+            { hasEspresso && <li>에스프레소</li> }
+          </ul>
+          <p>합계 : { formatter.format(sum) }원</p>
+        </dl>
     </div>
   </>
 }
