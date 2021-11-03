72593ff7028ffda8976ceb42740a298e2aea7f94
Jihye <kimji9663@nate.com>
2021-11-03 19:13:56 +0900

    NEXTJS 입문자를 위한 4주 강의 1주차

--- caffe/pages/order.js
+++ caffe/pages/order.js
@@ -1,17 +1,26 @@
 import Link from 'next/link';
 import Header from '../components/Header';
-import { useState, useMemo } from 'react';
+import { useState, useMemo, Fragment } from 'react';
 
 // 한화 화폐 단위 포멧(세자릿수마다 콤마)
 const formatter = Intl.NumberFormat( 'ko-KR' );
 
+const data = [
+  {name: '에스프레소',  price: 2800},
+  {name: '아메리카노',  price: 3000},
+  {name: '카페라떼',  price: 3500},
+];
+
+
 // 상태 State
 export default function Order() {
 
-  // [ 일기전용, 쓰기전용 ] = useState( 기본값 );
+  // [ 읽기전용, 쓰기전용 ] = useState( 기본값 );
   const [ hasEspresso, setEspresso ] = useState( false );
   const [ hasAmericano, setAmericano ] = useState( false );
   const [ hasLatte, setLatte ] = useState( false );
+  const [ selected, setSelected ] = useState( [] );
+  console.log( 'selected', selected );
 
   const sum = useMemo( () => {
     console.log( 'useMemo 실행' );
@@ -20,37 +29,45 @@ export default function Order() {
     value += hasAmericano ? 3000 : 0;
     value += hasLatte ? 3500 : 0;
     return value;
-  }, [ hasEspresso, hasAmericano, hasLatte ] );
+  }, [ selected ] );
 
   return <>
     <div className="container">
         <Header/>
         <h1 className="font-bold">Order</h1>
-
         <h2 className="text-xl font-bold">메뉴판</h2>
-
+        
         <dl>
-          <dt>에스프레소</dt>
-          <dd>2,800원 
-            <small><button onClick={ () => setEspresso( !hasEspresso ) } type="button">[ { hasEspresso ? '선택 해제' : '선택'} ]</button></small>
-          </dd>
+          {
+            data.map( element => (
+              <Fragment key={ element.name }>
+                <dt>{ element.name }</dt>
+                <dd>
+                  { formatter.format( element.price ) }원
 
-          <dt>아메리카노</dt>
-          <dd>3,000원 
-            <small><button onClick={ () => setAmericano( !hasAmericano ) } type="button">[ { hasAmericano ? '선택 해제' : '선택'} ]</button></small>
-          </dd>
-
-          <dt>카페라떼</dt>
-          <dd>3,500원 
-            <small><button onClick={ () => setLatte( !hasLatte ) } type="button">[ { hasLatte ? '선택 해제' : '선택'} ]</button></small>
-          </dd>
+                  <small>
+                    <button onClick={ () => {
+                      if( selected.includes( element ) ){
+                        setSelected( selected.filter( item => item !== element ) );
+                      }
+                      else {
+                        setSelected( [...selected, element] ); // element는 { name: '에스프레소', price: 2800 }
+                      }
+                    }}>
+                      [ { selected.includes( element ) ? '선택 해제' : '선택' } ]
+                    </button>
+                  </small>
+                </dd>
+              </Fragment>
+            ) )
+          }
         </dl>
 
         <hr/>
 
         <h2 className="text-xl font-bold">주문서</h2>
         <ul className="list-unstyled">
-          { hasEspresso && <li>에스프레소</li> }
+          { selected.map( item => <li key={ item.name }>{ item.name }</li> ) }
         </ul>
         <p>합계 : { formatter.format(sum) }원</p>
 
